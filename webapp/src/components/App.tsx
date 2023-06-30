087f285df225a177f0e36291b1dce2f9674791d2
React4Ever <latifalex95@gmail.com>
2023-06-30 16:02:26 +0200

    init project 2

--- webapp/src/components/App.tsx
+++ webapp/src/components/App.tsx
@@ -1,32 +1,31 @@
 import React, { useMemo } from 'react';
-import '../../public/assets/css/app.css'
+import '../../public/assets/css/app.css';
 import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import { Logger } from 'simple-logging-system';
-import Home from './pages/Home';
 import Layout from './layout/Layout';
 import ErrorPage from './pages/ErrorPage';
-import Row from "./layout/Row";
-import TopOfMoviesByGenre from "../services/sample/movie/TopMoviesByGenre";
+import Row from './layout/Row';
+import TopOfMoviesByGenre from '../services/sample/movie/TopMoviesByGenre';
 
 const logger = new Logger('App');
 
 export default function App() {
   const router = useMemo(() => createBrowserRouter([
     {
-      path:'/',
+      path: '/',
       element: <Layout><Outlet/></Layout>,
-      errorElement: <ErrorPage />,
-      children:[
+      errorElement: <ErrorPage/>,
+      children: [
         {
-        index: true,
-        element: <div>
-          <Row title={"NETFLIX ORIGINALS"} fetchMovies={TopOfMoviesByGenre(28)} isLargerRow={true}/>
-        </div>
-      }
-      ]
-    }
+          index: true,
+          element: <div>
+            <Row title={'NETFLIX ORIGINALS'} fetchMovies={TopOfMoviesByGenre(28)} isLargerRow={true}/>
+          </div>,
+        },
+      ],
+    },
   ]), []);
 
   logger.info('Render App');
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router}/>;
 }
