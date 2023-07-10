270efdf0b1d020a762bfc73b5ac7eb94eaa4d1a9
React4Ever <latifalex95@gmail.com>
2023-07-10 14:41:30 +0200

    request to db to get trailer

--- src/main/java/com/netchill/api/moviedb/models/Trailer.java
+++ src/main/java/com/netchill/api/moviedb/models/Trailer.java
@@ -1,5 +1,6 @@
 package com.netchill.api.moviedb.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Trailer {
     private Integer id;
     private List<ResultTrailer> results;
