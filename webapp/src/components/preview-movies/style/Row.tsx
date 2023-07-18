7f7e21d47cf0f7dee0db6242288474233f3c1910
MaryaLatif <latifmarya@gmail.com>
2023-07-19 01:18:51 +0200

    add top 3 to top rated movies

--- webapp/src/components/preview-movies/style/Row.tsx
+++ webapp/src/components/preview-movies/style/Row.tsx
@@ -7,11 +7,15 @@ import ShowTrailer from './streaming/trailer/ShowTrailer';
 import StreamingService from '../../../services/streaming/StreamingService';
 import { MediaType, Movie, Trailer } from '../../../api/types/MovieDbTypes';
 import RowLoading from './loading/RowLoading';
+import top1 from '../../../../assets/icons/top1.png';
+import top2 from '../../../../assets/icons/top2.png';
+import top3 from '../../../../assets/icons/top3.png';
 
 type Props = {
   title: string,
   movieList: Movie[],
   isLargerRow?: boolean,
+  topRated?: boolean,
   isDataLoading?: boolean
 };
 
@@ -23,7 +27,7 @@ type MovieInfo = {
 };
 
 function Row({
-  title, movieList, isLargerRow, isDataLoading,
+  title, movieList, isLargerRow, topRated, isDataLoading,
 }: Props) {
   const streamingService = getGlobalInstance(StreamingService);
 
@@ -33,6 +37,9 @@ function Row({
 
   const movieLoader = useLoader();
 
+  const top: any[] = [top1, top2, top3];
+
+  let i = 0;
   function handleClick(movieId: number, movieResume: string, type: MediaType, genreIds: number[]): void {
     setMovieInfo({
       id: movieId,
@@ -72,7 +79,7 @@ function Row({
               {movieList.map((movie) => (
                 <div
                   key={movie.id}
-                  className={classNames('row_poster', { row_poster_large: isLargerRow })}
+                  className={classNames('row_poster', { row_poster_large: isLargerRow }, { top_rated: topRated })}
                   onClick={() => handleClick(
                     movie.id,
                     movie.overview,
@@ -81,11 +88,13 @@ function Row({
                   )}
                   aria-hidden="true"
                 >
+                  {topRated && i < 3 && (<img src={top[i]} alt={'top 1'} className={'top_rated_img'}/>)}
+                  {i < 3 && <noscript>{i++}</noscript>}
                   <img
-                    src={(isLargerRow || !movie.backdrop_path ? movie.poster_path : movie.backdrop_path)}
-                    alt={movie.title}/>
+                      src={(isLargerRow || !movie.backdrop_path ? movie.poster_path : movie.backdrop_path)}
+                      alt={movie.title}/>
                   <div className={'info'}>
-                  <h3 className={'title'}>{movie.title ? movie.title : movie.name}</h3>
+                    <h3 className={'title'}>{movie.title ? movie.title : movie.name}</h3>
                     <p>Recommendation : {movie.vote_average}%</p>
                   </div>
                 </div>
