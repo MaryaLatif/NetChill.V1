2c4852e7f3552e5d84f6b7195b134a704ccd7a33
MaryaLatif <latifmarya@gmail.com>
2023-08-22 12:37:14 +0200

    - quick changing

--- webapp/src/components/pages/stream/Stream.tsx
+++ webapp/src/components/pages/stream/Stream.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import '../../../../assets/scss/components/general/streaming/stream/stream.scss';
 
-function Stream({ mediaName }:{ mediaName: string }) {
+function Stream({ mediaName }: { mediaName: string }) {
   return (
     <div className="stream-container">
-      <video id="videoPlayer" width="50%" controls muted={true} autoPlay>
-        <source src="http://localhost:8080/api/stream/video/test" type="video/MP4"/>
+      <video src="http://localhost:8080/api/stream/video/test" width="50%" controls autoPlay>
       </video>
     </div>
   );
