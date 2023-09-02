ceccd848038bbdc35eda6c66e5723d86502e7339
MaryaLatif <latifmarya@gmail.com>
2023-09-02 12:39:19 +0200

    - create IconButton component

--- webapp/src/components/general/search/SearchBar.tsx
+++ webapp/src/components/general/search/SearchBar.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import '../../../../assets/scss/components/general/search/search.scss';
 import { Search } from 'react-feather';
+import IconButton from '../../theme/action/IconButton';
 
 function SearchBar() {
   return (
-    <div className='search-bar'>
-      <input type='search' id='site-search' placeholder='Search'/>
-
-      {/* TODO passer par un composant custom IconButton */}
-        <button><Search className='search-bar__icon'/></button>
-        </div>
+    <div className="search-bar">
+      <input type="search" id="site-search" placeholder="Search" />
+      <IconButton><Search className="search-bar__icon" /></IconButton>
+    </div>
   );
 }
 
