e5aaab8c850f8e9fa5c22634f301a08c9b3b4e57
MaryaLatif <latifmarya@gmail.com>
2023-09-26 20:42:51 +0200

    remove useless @

--- src/main/java/com/netchill/api/moviedb/enums/ProductionType.java
+++ src/main/java/com/netchill/api/moviedb/enums/ProductionType.java
@@ -1,10 +1,5 @@
 package com.netchill.api.moviedb.enums;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public enum ProductionType {
     MOVIE,
     SERIE
