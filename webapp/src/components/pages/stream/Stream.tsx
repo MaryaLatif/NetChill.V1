d41e151a5656e16526f2c645b78427af8967c4b8
MaryaLatif <latifmarya@gmail.com>
2023-11-01 18:16:30 +0100

    change to serveur base url movies

--- webapp/src/components/pages/stream/Stream.tsx
+++ webapp/src/components/pages/stream/Stream.tsx
@@ -8,7 +8,8 @@ function Stream() {
   return (
     <div className="stream-container">
       {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-      <video src={`http://localhost:8080/api/stream/video/${movieId}`} width='100%' height='100%' controls autoPlay>
+      <video src={`${window.location.protocol}//${window.location.host}/api/stream/video/${movieId}`}
+             width='100%' height='100%' controls autoPlay>
       </video>
     </div>
   );
